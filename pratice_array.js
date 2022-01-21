81a2d0bcfacdea457d970180d293aea1a30dadf0
DESKTOP-F7V67DT\cuong <domanhcuong250701@gmail.com>
2022-01-22 00:29:02 +0700

    paractice day5

--- pratice_array.js
+++ pratice_array.js
@@ -57,3 +57,100 @@ myColor = (i) => {
   }
 };
 myColor(["Red", "Green", "White", "Black"]);
+
+// 6. Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.
+myFunction = (i) => {
+  if (typeof i === "string") {
+    const convertString = i.toString();
+    const stringToArray = convertString.split("");
+    let result = stringToArray[0];
+    for (let i = 1; i < stringToArray.length; i++) {
+      if (
+        Number(stringToArray[i]) % 2 == 0 &&
+        Number(stringToArray[i - 1]) % 2 == 0
+      ) {
+        result += "-" + stringToArray[i];
+      } else {
+        result += stringToArray[i];
+      }
+    }
+    console.log(result);
+  }
+};
+myFunction("025468");
+// 7. Write a JavaScript program to sort the items of an array.
+sortArray = (i) => {
+  if (Array.isArray(i)) {
+    const sortNumber = i.sort((a, b) => a - b);
+    console.log(sortNumber);
+  }
+};
+sortArray([3, 8, 7, 6, 5, -4, -3, 2, 1]);
+// 8. Write a JavaScript program to find the most frequent item of an array.
+frequentItem = (array) => {
+  if (Array.isArray(array)) {
+    const arrayOutput = [];
+    for (let i = 0; i < array.length; i++) {
+      if (!arrayOutput.includes(array[i])) {
+        arrayOutput.push(array[i]);
+      }
+    }
+    console.log(arrayOutput);
+    let arrayListLength = [];
+    let item = "";
+    let count = 0;
+    for (let i = 0; i < arrayOutput.length; i++) {
+      const filter = array.filter((item) => item == arrayOutput[i]);
+      const listLength = filter.length;
+      if (listLength > count) {
+        count = listLength;
+        item = filter[0];
+      }
+      arrayListLength.push(listLength);
+    }
+    console.log(count, item);
+  }
+};
+frequentItem([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]);
+
+// 9. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+swapCase = (i) => {
+  if (typeof i === "string") {
+    const string = i.trim();
+    const stringToArray = string.split(" ");
+    let result = "";
+    for (let i = 0; i < stringToArray.length; i++) {
+      const letter = stringToArray[i].split("");
+      // console.log(letter);
+      for (let j = 0; j < letter.length; j++) {
+        // console.log(letter[j]);
+        if (letter[j] != letter[j].toUpperCase()) {
+          result += letter[j].toUpperCase();
+        } else {
+          result += letter[j].toLowerCase();
+        }
+      }
+      result += " ";
+    }
+    console.log(result.trim());
+  }
+};
+swapCase("The Quick Brown Fox");
+
+// 10. Write a JavaScript program which prints the elements of the following array.
+arrayTest = (array) => {
+  if (Array.isArray(array)) {
+    let result = "";
+    for (let i = 0; i < array.length; i++) {
+      result += `row ${i}: ${array[i].join(", ")}\n`;
+    }
+    console.log(result);
+  }
+};
+arrayTest([
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+]);
